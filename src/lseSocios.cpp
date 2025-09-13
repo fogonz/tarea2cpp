40bdf7bba9a0c8b013b046261101070fc15b22ac
santiago-cardozo <santiagocardozo529@gmail.com>
2025-09-12 23:19:10 -0300

    todos los modulos restantes menos insertar
    todos los modulos restantes menos insertar

--- src/lseSocios.cpp
+++ src/lseSocios.cpp
@@ -33,14 +33,13 @@ void imprimirTLSESocios(TLSESocios lseSocios){
 void liberarTLSESocios(TLSESocios &lseSocios){
     if (lseSocios == NULL) return;
 
-    rep_lseSocios* actual = lseSocios->sig;
-    while (actual != NULL){
-        actual = actual->sig;
+	rep_lseSocios* actual;
+    while (lseSocios != NULL){
+        actual = lseSocios;
+		lseSocios = lseSocios->sig;
         liberarTSocio(actual->socio);
+		delete(actual);
     }
-
-    delete lseSocios;
-    lseSocios = NULL;
 }
 
 void insertarTLSESocios(TLSESocios &lseSocios, TSocio socio){
@@ -56,17 +55,61 @@ bool existeSocioTLSESocios(TLSESocios lseSocios, int ci){
 }
 
 TSocio obtenerSocioTLSESocios(TLSESocios lseSocios, int ci){
-
-    return NULL;
+	if (existeSocioTLSESocios(lseSocios, ci) == true) {
+		rep_lseSocios* actual = lseSocios;
+		while (ciTSocio(actual->socio) != ci) {
+			actual = actual->sig;
+		}
+    	return actual->socio;
+	}
+	return NULL;
 }
 
 TSocio obtenerNesimoTLSESocios(TLSESocios lseSocios, int n){
+	if (n > 0 && (nat)n <= cantidadTLSESocios(lseSocios)) {
+		int cont = 1;
+		rep_lseSocios* actual = lseSocios;
+		while (cont != n) {
+			actual = actual->sig;
+			cont++;
+		}
+
+		return actual->socio;
+	}
+
 	return NULL;
 }
 
 nat cantidadTLSESocios(TLSESocios lseSocios){
-	return 0;
+	nat cantidadSocios = 0;
+	rep_lseSocios* actual = lseSocios;
+	while (actual != NULL) {
+		actual = actual->sig;
+		cantidadSocios++;
+	}
+
+	return cantidadSocios;
 }
 
 void removerSocioTLSESocios(TLSESocios &lseSocios, int ci){
+	if (existeSocioTLSESocios(lseSocios, ci) == true) {
+		//si el primer nodo es el buscado, directamente lo borramos 
+		if (ciTSocio(lseSocios->socio) == ci) {
+			rep_lseSocios* actual = lseSocios;
+			lseSocios = lseSocios->sig;
+			liberarTSocio(actual->socio);
+			delete actual;
+		} else {
+			//si no era el primero no queda de otra que buscarlo
+			rep_lseSocios* busqueda = lseSocios->sig;
+			rep_lseSocios* anterior = lseSocios;
+			while (ciTSocio(busqueda->socio) != ci) {
+				busqueda = busqueda->sig;
+				anterior = anterior->sig;
+			}
+			anterior->sig = busqueda->sig;
+			liberarTSocio(busqueda->socio);
+			delete busqueda;
+		}	
+	}
 }
